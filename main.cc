2231b335d212a756a6d1650c8ac1245f66c178f6
Paweł Stiasny <pawelstiasny@gmail.com>
2012-12-05 02:52:23 +0100

    Remove unused TestTriangle class.

--- main.cc
+++ main.cc
@@ -15,24 +15,6 @@
 GraphNode* scene = NULL;
 std::list<Animation*> animations;
 
-class TestTriangle : public GraphNode
-{
-private:
-
-public:
-	virtual void doRender()
-	{
-		glBegin(GL_TRIANGLES);					
-			glColor3f(0.0f,0.0f,1.0f);			
-			glVertex3f( 0.0f, 1.0f, 0.0f);		
-			glColor3f(0.0f,1.0f,0.0f);			
-			glVertex3f(-1.0f,-1.0f, 0.0f);		
-			glColor3f(1.0f,0.0f,0.0f);			
-			glVertex3f( 1.0f,-1.0f, 0.0f);		
-		glEnd();
-	}
-};
-
 class TestSceneRotation : public Animation
 {
 private:
