3da89b305131651d385cb63d7aaa4e9adb8ef733
Paweł Stiasny <pawelstiasny@gmail.com>
2012-12-24 04:16:50 +0100

    Merge back the original fragment shader.

--- shaders/default.fs
+++ shaders/default.fs
@@ -1,24 +1,18 @@
-#version 120
+#version 130
 
-//uniform mat4x4 MVP;
-//uniform mat4x4 MV;
-//uniform mat3x3 NormalMx;
 uniform sampler2D tex_sampler;
 in vec4 pos;
 in vec4 light_pos;
 in vec3 normal;
 
 void main() {
-	//gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
-    gl_FragColor = texture2D(tex_sampler, gl_TexCoord[0].st);
-    /*
+	//vec4 light_dir =light_pos - pos);
 	vec4 light_dir = light_pos;
 
 	vec4 diffuse = max(0.0, dot(normalize(normal), light_dir.xyz)) * gl_FrontMaterial.diffuse;
 	vec4 ambient = gl_FrontMaterial.ambient;
 
-	//vec4 texel = texture2D(tex_sampler, gl_TexCoord[0].st);
-    vec4 texel = vec4(1.0, 1.0, 1.0, 1.0);
-	gl_FragColor = (diffuse + ambient) * texel;*/
+	vec4 texel = texture2D(tex_sampler, gl_TexCoord[0].st);
+	gl_FragColor = (diffuse + ambient) * texel;
 }
 
