38770c332e9b9896faf4c7b849538ca17f870090
Paweł Stiasny <pawelstiasny@gmail.com>
2013-07-23 01:07:37 +0200

    Implement Poisson sampled shadows.

--- data/std.fs
+++ data/std.fs
@@ -1,5 +1,12 @@
 #version 130
 
+vec2 poissonDisk[4] = vec2[](
+  vec2( -0.94201624, -0.39906216 ),
+  vec2( 0.94558609, -0.76890725 ),
+  vec2( -0.094184101, -0.92938870 ),
+  vec2( 0.34495938, 0.29387760 )
+);
+
 float specular_term(vec3 normal, vec3 light, vec3 cam, float shininess)
 {
 	vec3 halfv = normalize(light + cam);
@@ -15,8 +22,14 @@ float diffuse_term(vec3 normal, vec3 light)
 
 float shadow_term(sampler2DShadow shadow_sampler, vec4 shadowspace_pos)
 {
-	float visibility =
-		textureProj(shadow_sampler, shadowspace_pos - vec4(0,0,0.02,0));
+	float visibility = 0.0;
+	for (int i = 0; i < 4; i++) {
+		visibility += 0.25 * textureProj(
+			shadow_sampler,
+			shadowspace_pos
+				+ vec4(poissonDisk[i] / 700.0, 0.0, 0.0)
+				- vec4(0,0,0.02,0));
+	}
 
 	vec2 not_in_shadowspace = 
 		step(shadowspace_pos.xy, vec2(0.0,0.0)) +
